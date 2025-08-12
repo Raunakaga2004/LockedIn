89bd0721312f2538cf60a2ca31aa916a82eae1a3
Raunakaga2004 <raunakaga12@gmail.com>
2025-08-12 20:25:10 +0530

    added basic layout of habit routes)

--- backend/src/routes/habit.routes.ts
+++ backend/src/routes/habit.routes.ts
@@ -1,10 +1,25 @@
+import { Router } from "express";
+
+
+const router = Router();
+
 // createHabit
+router.post('/createHabit', (req, res)=>{
+  
+})
+
 // getHabit
 // getAllHabit
 // updateHabit
 // deleteHabit
 
 // at 12:01 AM we need to add the current habits to log using node-cron
+
+// updateHabitLog
+// deleteHabitLog
+// addHabitLog
+// getHabitLogs
+// getAllHabitLogs
 
 // get summary from logs
 // getHabitSummary
