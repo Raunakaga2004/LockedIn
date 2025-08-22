808f04a268501d5e5fb6fa366bef819de9260ac6
Raunakaga2004 <raunakaga12@gmail.com>
2025-08-22 16:55:12 +0530

    planned workout routes

---
+++ backend/src/routes/workout.route.ts
@@ -0,0 +1,49 @@
+// create workout plan
+
+// delete workout plan
+
+// get workout plan
+
+// get all workout plan
+
+// update workout plan (also add workout plan exercises)
+
+// add exercises to workout plan
+
+// delete exercise to workout plan
+
+// update exercise to workout plan
+
+// get exercise to workout plan
+
+// get all exercise to workout plan
+
+// add workout session log
+
+// update workout session log
+
+// read workout session log
+
+// read workout session log of particular workout
+
+// read all workout session log
+
+// delete workout session log
+
+// add workout exercise log
+
+// update workout exercise log
+
+// delete workout exercise log
+
+// read workout exercise log
+
+// read all workout exercise log 
+
+// get workout session summary
+
+// get workout plan summary 
+
+// get exercise log summary
+
+// get all summary
